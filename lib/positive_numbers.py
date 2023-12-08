fac950589dbd67cb9182d0ad89580b96aa74def3
beatricenjerimwangi <bbeatricemwangi@gmail.com>
2023-12-08 14:42:30 -0500

    completed

---
+++ lib/positive_numbers.py
@@ -0,0 +1,8 @@
+def positive_numbers(a ,b ,c):
+    if( a>0 and b>0):
+        return True
+    elif( a<0 and b<0):
+        return False
+print(positive_numbers(2,4,-3)) 
+print(positive_numbers(4,6,10) )
+print(positive_numbers(-1,-2,-4))  
