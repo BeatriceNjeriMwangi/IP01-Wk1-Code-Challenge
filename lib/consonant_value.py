533a0408a462b811f8466a4add29035c69d36b73
beatricenjerimwangi <bbeatricemwangi@gmail.com>
2023-12-10 16:59:15 -0500

    completed

--- lib/consonant_value.py
+++ lib/consonant_value.py
@@ -2,7 +2,7 @@ def check_highest_consonant(string):
     vowels = "aeiou"
     consonant_strings = []
     new_string = ""
-
+    #loop through
     for i in string.lower():
         if i not in vowels:
             new_string = new_string + i
@@ -10,10 +10,15 @@ def check_highest_consonant(string):
             if new_string:
                 consonant_strings.append(new_string)
                 new_string = ""
-
+    #check last substring of consonant
     if new_string:
         consonant_strings.append(new_string)
+    #calculate the value of each consonant string
+    values = [sum(ord(c) - ord('a') + 1 for c in substring) for substring in consonant_strings]
 
-    return max(check_highest_consonant(string) for string in consonant_string)
+
+    #return highest consonant count
+    return max(values, default=0)
 
 print (check_highest_consonant("Animal"))
+print (check_highest_consonant("Butterfly"))
